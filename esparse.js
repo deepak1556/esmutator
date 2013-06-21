205c6079dcb2741995bb764ceb7e529a0a0b5175
robo <hop2deep@gmail.com>
2013-06-21 09:02:00 +0530

    token generator

--- esparse.js
+++ esparse.js
@@ -10,4 +10,23 @@
     'use strict';
 
     var options, sourceFile, input, inputLen;
+
+    function setOptions(opts) {
+	options = opts || {};
+	for (var opt in defaultOptions) if (!Object.prototype.hasOwnProperty.call(options, opt))
+	    options[opt] = defaultOptions[opt];
+	sourceFile = options.sourceFile || null;
+    }
+
+    var getLineInfo = exports.getLineInfo = function(input, offset) {
+	for (var line = 1, cur = 0;;) {
+	    lineBreak.lastIndex = cur;
+	    var match = lineBreak.exec(input);
+	    if (match && match.index < offset) {
+		++line;
+		cur = match.index + match[0].length;
+	    } else break;
+	}
+	return {line: line, column: offset - cur};
+    };
 }));
