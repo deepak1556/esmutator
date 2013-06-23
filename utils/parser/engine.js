38fc1a9e7eeb69ddc32137497f7130984c786412
robo <hop2deep@gmail.com>
2013-06-23 08:57:06 +0530

    tracking line locations , rewriting  parser engine

--- utils/parser/engine.js
+++ utils/parser/engine.js
@@ -1,534 +1,183 @@
+function(exports, opts) {
+    "use strict";
 
-// parse.js
-// Parser for Simplified JavaScript written in Simplified JavaScript
-// From Top Down Operator Precedence
-// http://javascript.crockford.com/tdop/index.html
-// Douglas Crockford
-// 2010-06-26
+    var options, input, inputLen, record;
 
-var make_parse = function() {
-    var scope;
-    var symbol_table = {};
-    var token;
-    var tokens;
-    var token_nr;
+    var defaultOptions = exports.defaultOptions = {
+	strictSemicolons: false,
+	allowTrailingCommas: true,
+	forbidReserved: false,
+	locations: false,
+	onComment: null,
+	ranges: false,
+	existingTree: null,
+	record: null
+    }
 
-    var itself = function() {
-        return this;
-    };
+    exports.parse = function(inpt, opts) {
+	input = String(inpt);
+	inputLen = input.length;
+	setOptions(opts);
+	initToken();
+	return parseTopLevel(options.existingTree);
+    }
 
-    var original_scope = {
-        define: function(n) {
-            var t = this.def[n.value];
-            if (typeof t === "object") {
-                n.error(t.reserved ? "Already reserved." : "Already defined.");
-            }
-            this.def[n.value] = n;
-            n.reserved = false;
-            n.nud = itself;
-            n.led = null;
-            n.std = null;
-            n.lbp = 0;
-            n.scope = scope;
-            return n;
-        },
-        find: function(n) {
-            var e = this,
-            o;
-            while (true) {
-                o = e.def[n];
-                if (o && typeof o !== 'function') {
-                    return e.def[n];
-                }
-                e = e.parent;
-                if (!e) {
-                    o = symbol_table[n];
-                    return o && typeof o !== 'function' ? o : symbol_table["(name)"];
-                }
-            }
-        },
-        pop: function() {
-            scope = this.parent;
-        },
-        reserve: function(n) {
-            if (n.arity !== "name" || n.reserved) {
-                return;
-            }
-            var t = this.def[n.value];
-            if (t) {
-                if (t.reserved) {
-                    return;
-                }
-                if (t.arity === "name") {
-                    n.error("Already defined.");
-                }
-            }
-            this.def[n.value] = n;
-            n.reserved = true;
-        }
-    };
+    function setOptions(opts) {
+	options = opts || {};
+	for(var opt in defaultOptions) {
+	    if(!Object.prototype.hasOwnProperty.call(options, opt)) {
+		options[opt] = defaultOptions[opt];
+	    }
+	}
+	record = options.record || null;
+    }
 
-    var new_scope = function() {
-        var s = scope;
-        scope = Object.create(original_scope);
-        scope.def = {};
-        scope.parent = s;
-        return scope;
-    };
+    var getLineInfo = exports.getLineInfo = function(input, offset) {
+	for(var line=1, cur = 0;;) {
+	    lineBreak.lastIndex = cur;
+	    var match = lineBreak.exec(input);
+	    if(match && match.index < offset) {
+		++line;
+		cur = match.index + match[0].length;
+	    }else {
+		break;
+	    }   
+	}
 
-    var advance = function(id) {
-        var a, o, t, v;
-        if (id && token.id !== id) {
-            token.error("Expected '" + id + "'.");
-        }
-        if (token_nr >= tokens.length) {
-            token = symbol_table["(end)"];
-            return;
-        }
-        t = tokens[token_nr];
-        token_nr += 1;
-        v = t.value;
-        a = t.type;
-        if (a === "name") {
-            o = scope.find(v);
-        } else if (a === "operator") {
-            o = symbol_table[v];
-            if (!o) {
-                t.error("Unknown operator.");
-            }
-        } else if (a === "string" || a === "number") {
-            o = symbol_table["(literal)"];
-            a = "literal";
-        } else {
-            t.error("Unexpected token.");
-        }
-        token = Object.create(o);
-        token.from = t.from;
-        token.to = t.to;
-        token.value = v;
-        token.arity = a;
-        return token;
-    };
+	return {line: line, column: offest - cur};
+    }
 
-    var expression = function(rbp) {
-        var left;
-        var t = token;
-        advance();
-        left = t.nud();
-        while (rbp < token.lbp) {
-            t = token;
-            advance();
-            left = t.led(left);
-        }
-        return left;
-    };
+    exports.tokenize = function(inpt, opts) {
+	input = String(inpt);
+	inputLen = input.length;
+	setOptions(opts);
+	initToken();
 
-    var statement = function() {
-        var n = token,
-        v;
+	var t = {};
+	function getToken(forcedregExp) {
+	    t = readToken(forcedRegToken);
+	    t.startLoc = tokstartloc;
+	    t.endLoc = tokendloc;
 
-        if (n.std) {
-            advance();
-            scope.reserve(n);
-            return n.std();
-        }
-        v = expression(0);
-        if (!v.assignment && v.id !== "(") {
-            v.error("Bad expression statement.");
-        }
-        advance(";");
-        return v;
-    };
+	    return t;
+	}
 
-    var statements = function() {
-        var a = [],
-        s;
-        while (true) {
-            if (token.id === "}" || token.id === "(end)") {
-                break;
-            }
-            s = statement();
-            if (s) {
-                a.push(s);
-            }
-        }
-        return a.length === 0 ? null : a.length === 1 ? a[0] : a;
-    };
+	getToken.jumpTo = function(pos, regallowed) {
+	    tokpos = pos;
+	    if(options.locations) {
+		tokcurline = toklinestart = lineBreak.lastIndex = 0;
+		var match;
+		while((match = lineBreak.exec(input) && match.index < pos)) {
+		    ++tokcurline;
+		    toklinestart = match.index + match[0].length;
+		}
+	    }
 
-    var block = function() {
-        var t = token;
-        advance("{");
-        return t.std();
-    };
+	    var ch = input.charAt(pos - 1);
+	    tokregexallowed = regallowed;
+	    skipSpace();
+	}
 
-    var original_symbol = {
-        nud: function() {
-            this.error("Undefined.");
-        },
-        led: function(left) {
-            this.error("Missing operator.");
-        }
-    };
+	return getToken;
+    }
 
-    var symbol = function(id, bp) {
-        var s = symbol_table[id];
-        bp = bp || 0;
-        if (s) {
-            if (bp >= s.lbp) {
-                s.lbp = bp;
-            }
-        } else {
-            s = Object.create(original_symbol);
-            s.id = s.value = id;
-            s.lbp = bp;
-            symbol_table[id] = s;
-        }
-        return s;
-    };
+    var tokpos; //current position of the tokenizer in the input
+    var tokstart, tokend; //start and end offsets of current token
+    var tokstartloc, tokendloc; //to hold objects containing tokens start and end line/column pairs 
+    var toktype, tokvalue; //type and value of current token
+    var tokregexallowed; //to distinguish between operators and regular expressions, check if the last token was allowed to be followed by an expression.
+    var tokcurline, toklinestart; //keep track of current line and check if a new line has been entered
+    var laststart, lastend, lastendloc; //info about the previous token useful when traversing a node
+    var inFunction, labels, strict; //check for function integrity
 
-    var constant = function(s, v) {
-        var x = symbol(s);
-        x.nud = function() {
-            scope.reserve(this);
-            this.value = symbol_table[this.id].value;
-            this.arity = "literal";
-            return this;
-        };
-        x.value = v;
-        return x;
-    };
+    function raise(pos, message) {
+	var loc = getLineInfo(input, pos);
+	message += " (" + loc.line + ":" + loc.column + ")";
+	var err = new SyntaxError(message);
+	err.pos = pos;
+	err.loc = loc;
+	err.raisedAt = tokpos;
+	throw err;
+    }
 
-    var infix = function(id, bp, led) {
-        var s = symbol(id, bp);
-        s.led = led || function(left) {
-            this.first = left;
-            this.second = expression(bp);
-            this.arity = "binary";
-            return this;
-        };
-        return s;
-    };
+    function predict(words) {
+	words = words.split(" ");
+	var f = "", cats = [];
+  	out: {
+	    for(var i = 0; i < words.length; i++) {
+		for(var j = 0; j < cats.length; j++) {
+		    if(cats[j][0].length == words[i].length) {
+			cats[j].push(words[i]);
+			continue out;
+		    }
+		    cats.push([words[i]]);
+		}
+	    }
+	}
 
-    var infixr = function(id, bp, led) {
-        var s = symbol(id, bp);
-        s.led = led || function(left) {
-            this.first = left;
-            this.second = expression(bp - 1);
-            this.arity = "binary";
-            return this;
-        };
-        return s;
-    };
+	function  compare(arr) {
+	    if(arr.length == 1) {
+		return f += "return str === " + JSON.stringify(arr[0]) + ";";
+	    }
+	    f += "switch(arr){";
+	    for(var i = 0; i < arr.length; i++) {
+		f += "case " + JSON.stringify(arr[i]) + ":";
+	    }
+	    f += "return true}return false;";
+	}
 
-    var assignment = function(id) {
-        return infixr(id, 10, function(left) {
-            if (left.id !== "." && left.id !== "[" && left.arity !== "name") {
-                left.error("Bad lvalue.");
-            }
-            this.first = left;
-            this.second = expression(9);
-            this.assignment = true;
-            this.arity = "binary";
-            return this;
-        });
-    };
+	if(cats.length > 3) {
+	    cats.sort(function(a, b) {
+		return b.length - a.length; 
+	    });
+	    f += "switch(str.length){";
+	    for(var i = 0; i < cats.length; i++) {
+		var cat = cats[i];
+		f += "case " + cat[0].length + ":";
+		compare(cat);
+	    }
+	    f += "}";
+	}else {
+	    compare(words);
+	}
 
-    var prefix = function(id, nud) {
-        var s = symbol(id);
-        s.nud = nud || function() {
-            scope.reserve(this);
-            this.first = expression(70);
-            this.arity = "unary";
-            return this;
-        };
-        return s;
-    };
+	return new Function("str", f);
+    }
+    
+    //ecmascript 3 reserved wordlist
+    var reservedWord3 = predict("abstract boolean byte char class double enum export extends final float goto implements import int interface long native package private protected public short static super synchronized throws transient volatile");
 
-    var stmt = function(s, f) {
-        var x = symbol(s);
-        x.std = f;
-        return x;
-    };
+    //ecmascrpt5 reserved wordlist
+    var reservedWord5 = predict("class enum extends super const export import");
 
-    symbol("(end)");
-    symbol("(name)");
-    symbol(":");
-    symbol(";");
-    symbol(")");
-    symbol("]");
-    symbol("}");
-    symbol(",");
-    symbol("else");
+    //reserved words in strict mode
+    var strictreservedWord = predict("implements interface let package private protected public static yield");
 
-    constant("true", true);
-    constant("false", false);
-    constant("null", null);
-    constant("pi", 3.141592653589793);
-    constant("Object", {});
-    constant("Array", []);
+    //frbidden variables in strict mode
+    var strictbadword = predict("eval arguements");
 
-    symbol("(literal)").nud = itself;
+    var keyword = predict("break case catch continue debugger default do else finally for function if return switch throw try var while with null true false instanceof typeof void delete new in this");
 
-    symbol("this").nud = function() {
-        scope.reserve(this);
-        this.arity = "this";
-        return this;
-    };
+    function line_loc_t() {
+	this.line = tokendline;
+	this.column = tokpos - toklinestart;
+    }
 
-    assignment("=");
-    assignment("+=");
-    assignment("-=");
+    function initToken() {
+	tokcurline = 1;
+	tokpos = toklinestart = 0;
+	tokregexpallowed = true;
+	skipSpace();
+    }
 
-    infix("?", 20, function(left) {
-        this.first = left;
-        this.second = expression(0);
-        advance(":");
-        this.third = expression(0);
-        this.arity = "ternary";
-        return this;
-    });
-
-    infixr("&&", 30);
-    infixr("||", 30);
-
-    infixr("===", 40);
-    infixr("!==", 40);
-    infixr("<", 40);
-    infixr("<=", 40);
-    infixr(">", 40);
-    infixr(">=", 40);
-
-    infix("+", 50);
-    infix("-", 50);
-
-    infix("*", 60);
-    infix("/", 60);
-
-    infix(".", 80, function(left) {
-        this.first = left;
-        if (token.arity !== "name") {
-            token.error("Expected a property name.");
-        }
-        token.arity = "literal";
-        this.second = token;
-        this.arity = "binary";
-        advance();
-        return this;
-    });
-
-    infix("[", 80, function(left) {
-        this.first = left;
-        this.second = expression(0);
-        this.arity = "binary";
-        advance("]");
-        return this;
-    });
-
-    infix("(", 80, function(left) {
-        var a = [];
-        if (left.id === "." || left.id === "[") {
-            this.arity = "ternary";
-            this.first = left.first;
-            this.second = left.second;
-            this.third = a;
-        } else {
-            this.arity = "binary";
-            this.first = left;
-            this.second = a;
-            if ((left.arity !== "unary" || left.id !== "function") && left.arity !== "name" && left.id !== "(" && left.id !== "&&" && left.id !== "||" && left.id !== "?") {
-                left.error("Expected a variable name.");
-            }
-        }
-        if (token.id !== ")") {
-            while (true) {
-                a.push(expression(0));
-                if (token.id !== ",") {
-                    break;
-                }
-                advance(",");
-            }
-        }
-        advance(")");
-        return this;
-    });
-
-
-    prefix("!");
-    prefix("-");
-    prefix("typeof");
-
-    prefix("(", function() {
-        var e = expression(0);
-        advance(")");
-        return e;
-    });
-
-    prefix("function", function() {
-        var a = [];
-        new_scope();
-        if (token.arity === "name") {
-            scope.define(token);
-            this.name = token.value;
-            advance();
-        }
-        advance("(");
-        if (token.id !== ")") {
-            while (true) {
-                if (token.arity !== "name") {
-                    token.error("Expected a parameter name.");
-                }
-                scope.define(token);
-                a.push(token);
-                advance();
-                if (token.id !== ",") {
-                    break;
-                }
-                advance(",");
-            }
-        }
-        this.first = a;
-        advance(")");
-        advance("{");
-        this.second = statements();
-        advance("}");
-        this.arity = "function";
-        scope.pop();
-        return this;
-    });
-
-    prefix("[", function() {
-        var a = [];
-        if (token.id !== "]") {
-            while (true) {
-                a.push(expression(0));
-                if (token.id !== ",") {
-                    break;
-                }
-                advance(",");
-            }
-        }
-        advance("]");
-        this.first = a;
-        this.arity = "unary";
-        return this;
-    });
-
-    prefix("{", function() {
-        var a = [],
-        n, v;
-        if (token.id !== "}") {
-            while (true) {
-                n = token;
-                if (n.arity !== "name" && n.arity !== "literal") {
-                    token.error("Bad property name.");
-                }
-                advance();
-                advance(":");
-                v = expression(0);
-                v.key = n.value;
-                a.push(v);
-                if (token.id !== ",") {
-                    break;
-                }
-                advance(",");
-            }
-        }
-        advance("}");
-        this.first = a;
-        this.arity = "unary";
-        return this;
-    });
-
-
-    stmt("{", function() {
-        new_scope();
-        var a = statements();
-        advance("}");
-        scope.pop();
-        return a;
-    });
-
-    stmt("var", function() {
-        var a = [],
-        n, t;
-        while (true) {
-            n = token;
-            if (n.arity !== "name") {
-                n.error("Expected a new variable name.");
-            }
-            scope.define(n);
-            advance();
-            if (token.id === "=") {
-                t = token;
-                advance("=");
-                t.first = n;
-                t.second = expression(0);
-                t.arity = "binary";
-                a.push(t);
-            }
-            if (token.id !== ",") {
-                break;
-            }
-            advance(",");
-        }
-        advance(";");
-        return a.length === 0 ? null : a.length === 1 ? a[0] : a;
-    });
-
-    stmt("if", function() {
-        advance("(");
-        this.first = expression(0);
-        advance(")");
-        this.second = block();
-        if (token.id === "else") {
-            scope.reserve(token);
-            advance("else");
-            this.third = token.id === "if" ? statement() : block();
-        } else {
-            this.third = null;
-        }
-        this.arity = "statement";
-        return this;
-    });
-
-    stmt("return", function() {
-        if (token.id !== ";") {
-            this.first = expression(0);
-        }
-        advance(";");
-        if (token.id !== "}") {
-            token.error("Unreachable statement.");
-        }
-        this.arity = "statement";
-        return this;
-    });
-
-    stmt("break", function() {
-        advance(";");
-        if (token.id !== "}") {
-            token.error("Unreachable statement.");
-        }
-        this.arity = "statement";
-        return this;
-    });
-
-    stmt("while", function() {
-        advance("(");
-        this.first = expression(0);
-        advance(")");
-        this.second = block();
-        this.arity = "statement";
-        return this;
-    });
-
-    return function(source) {
-        tokens = source.tokens('=<>!+-*&|/%^', '=<>&|');
-        token_nr = 0;
-        new_scope();
-        advance();
-        var s = statements();
-        advance("(end)");
-        scope.pop();
-        return s;
-    };
-}; 
+    function finishToken(type, val) {
+	tokend = tokpos;
+	if(option.locations) {
+	    tokendloc = new line_loc_t;
+	}
+	toktype = type;
+	skipSpace();
+	tokval = val;
+	tokregexallowed = type.beforeExpr;
+    }
+}
